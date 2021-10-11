fa3ea1aa2419dcba2d60fe85544928815b0c6d98
wenda.cao <wenda.cao@huifu.com>
2021-10-11 19:09:53 +0800

    launch

--- app/src/main/java/com/cwd/money/act/LaunchActivity.kt
+++ app/src/main/java/com/cwd/money/act/LaunchActivity.kt
@@ -1,7 +1,19 @@
 package com.cwd.money.act
 
+import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import com.cwd.money.R
+import com.cwd.money.databinding.ActivityLaunchBinding
+
 
 class LaunchActivity : AppCompatActivity() {
 
+    private lateinit var binding:ActivityLaunchBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLaunchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
 }
