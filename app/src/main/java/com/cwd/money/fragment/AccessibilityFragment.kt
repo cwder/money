ada70d90d3efc911f5e4b463dbaa6eaab1c2f76c
曹闻达 <cwder@126.com>
2021-10-16 01:29:46 +0800

    fragment

---
+++ app/src/main/java/com/cwd/money/fragment/AccessibilityFragment.kt
@@ -0,0 +1,31 @@
+package com.cwd.money.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cwd.money.databinding.FragAccessibilityBinding
+
+class AccessibilityFragment:Fragment() {
+
+    private var _bind:FragAccessibilityBinding? = null
+
+    private val bind
+                get() = _bind!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _bind = FragAccessibilityBinding.inflate(inflater)
+        val view = bind.root
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _bind = null
+    }
+}
---
+++ app/src/main/java/com/cwd/money/fragment/FavoriteFragment.kt
@@ -0,0 +1,6 @@
+package com.cwd.money.fragment
+
+import androidx.fragment.app.Fragment
+
+class FavoriteFragment:Fragment() {
+}
