cb87d66159d9b7868d3b9f2e9ee24000b62d1477
wenda.cao <wenda.cao@huifu.com>
2021-10-19 17:52:41 +0800

    优化http代码

--- app/src/main/java/com/cwd/money/network/IApiService.kt
+++ app/src/main/java/com/cwd/money/network/IApiService.kt
@@ -10,34 +10,13 @@ import retrofit2.http.GET
 
 interface IApiService
 {
-    @GET("share")
-    suspend fun requestGetShare(): Share
-
-    companion object{
-        private const val BASE_URL = "http://"
-        private var service:IApiService? = null
-
-
-        fun getApi():IApiService{
-            if(null == service){
-                val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
-                    this.level = HttpLoggingInterceptor.Level.BASIC
-                }
-
-                val client = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
-                val retrofit = Retrofit.Builder().baseUrl(BASE_URL).client(client)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-                service = retrofit.create(IApiService::class.java)
-            }
-            return service!!
-
-        }
-
-
-
-
+    companion object {
+        val service:IApiService by lazy {
+            ApiBuilder.build(IApiService::class.java)
+         }
     }
 
+    @GET("share")
+    suspend fun requestGetShare(): Share
 
 }
