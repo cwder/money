d0af97ddd2db0c5fac577d3fffb5f874bc41d619
曹闻达 <cwder@126.com>
2021-11-25 23:31:50 +0800

    building

--- app/src/main/java/com/cwd/money/act/page/SingleScreen.kt
+++ app/src/main/java/com/cwd/money/act/page/SingleScreen.kt
@@ -1,9 +1,36 @@
 package com.cwd.money.act.page
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SingleScreen(){
-    Text("SingleScreen")
+    MaterialTheme{
+        Column(
+            modifier = Modifier.fillMaxSize().padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Row{
+                Text(text = "第一个文本控件ikkokokojih呼呼呼呼呼呼")
+                Text(text = "   第二个文本控件")
+            }
+
+            Row{
+                Text(text = "第一个文本控件ikkokokojih呼呼呼呼呼呼")
+                Text(text = "   第二个文本控件")
+            }
+
+            Row{
+                Text(text = "第一个文本控件ikkokokojih呼呼呼呼呼呼")
+                Text(text = "   第二个文本控件")
+            }
+
+        }
+    }
 }
