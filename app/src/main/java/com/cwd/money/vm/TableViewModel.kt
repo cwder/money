9ff9ea95c4c1a5c8b30c624d3c83bcd36a33a285
caowenda <wenda.cao@huifu.com>
2022-03-26 23:46:27 +0800

    flow改造

--- app/src/main/java/com/cwd/money/vm/TableViewModel.kt
+++ app/src/main/java/com/cwd/money/vm/TableViewModel.kt
@@ -3,19 +3,27 @@ package com.cwd.money.vm
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
+import androidx.lifecycle.asLiveData
 import com.cwd.money.mysql.DBHelper
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class TableViewModel(application: Application): AndroidViewModel(application) {
 
-    var tables:LiveData<List<String>> = liveData<List<String>> {
-        withContext(Dispatchers.IO){
-            val tables = DBHelper.allTable()
-            emit(tables)
-        }
-    }
+//    var tables:LiveData<List<String>> = liveData<List<String>> {
+//        withContext(Dispatchers.IO){
+//            val tables = DBHelper.allTable()
+//            emit(tables)
+//        }
+//    }
+
+    var tables:LiveData<List<String>> = flow<List<String>>{
+                val tables = DBHelper.allTable()
+                emit(tables)
+            }.flowOn(Dispatchers.IO).catch { e-> e.printStackTrace() }
+            .asLiveData()
 
 
 
